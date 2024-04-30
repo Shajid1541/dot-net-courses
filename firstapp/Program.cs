9589aa440c06bde02fcb1d59d7a5580003e3b0a5
Shajid1541 <shajid.kamal@brainstation-23.com>
2024-04-30 15:12:37 +0600

    Add project files.

---
+++ firstapp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace firstapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+
+            Console.WriteLine("Hello, World!"+number);
+        }
+    }
+}
